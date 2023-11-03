ad5cf989924c2b621daa5bea67b0a9f8875d5ecb
HOONKH <boospa0305@gmai.com>
2023-11-03 14:19:12 +0900

    clock

--- main.js
+++ main.js
@@ -4,15 +4,24 @@ let seconds = 11;
 function getTime() {
   const date = new Date();
 
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
+  let amPm = "AM";
 
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  // let hours = String(date.getHours()).padStart(2, "0");
+  let hours = date.getHours();
+
+  if (hours >= 12) amPm = "PM";
+  if (hours >= 13) {
+    hours %= 12;
+  }
+  hours = String(hours).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  let seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${amPm} ${hours}:${minutes}:${seconds}`;
 
   seconds++;
 }
 getTime();
 setInterval(getTime, 1000);
 
-// new date -> 날짜 기능 모두 포함
+// new date -> 날짜 기능 모두 포함 객체
