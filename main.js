eb0a49f5b8415633f79f81897d0f75fdcbf052f1
HOONKH <boospa0305@gmai.com>
2023-11-03 17:58:29 +0900

    Quotes

--- main.js
+++ main.js
@@ -24,4 +24,43 @@ function getTime() {
 getTime();
 setInterval(getTime, 1000);
 
+const QUOTES_LIST = "quotesList";
 // new date -> 날짜 기능 모두 포함 객체
+
+function getQuotes() {
+  const quotes = document.querySelector(".quotes");
+
+  let savedQuotes = localStorage.getItem(QUOTES_LIST);
+
+  if (!savedQuotes) {
+    localStorage.setItem(
+      QUOTES_LIST,
+      JSON.stringify(["Continuous Improvement"])
+    );
+    savedQuotes = localStorage.getItem(QUOTES_LIST);
+  }
+  let parsedQuotes = JSON.parse(savedQuotes);
+  // 없으면 하나 생성
+  quotes.innerText =
+    parsedQuotes[Math.floor(Math.random() * parsedQuotes.length)];
+}
+getQuotes();
+
+function onClickNewQuotes() {
+  const quotes = document.querySelector(".quotes");
+  const newQuotesInput = document.querySelector(".new-quotes-input");
+
+  if (!newQuotesInput.value) return;
+
+  // 로컬 스토리지에 저장
+  let savedQuotes = localStorage.getItem(QUOTES_LIST);
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  parsedQuotes.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES_LIST, JSON.stringify(parsedQuotes));
+
+  // 현재 페이지 반영
+  quotes.innerText = newQuotesInput.value;
+  newQuoteInput.value = "";
+}
